760c6d9569b1b01e3ec25899bf6f94e9036bd6ec
jangg0627 <jangg0627@naver.com>
2023-04-27 23:17:52 +0900

    📐기본적인 레이아웃 설계

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Home page!</h1>
-    </main>
+    </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <p>"반가워요! 성장하는 개발자 Coovi의 블로그입니다!"</p>;
+}

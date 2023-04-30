d986c3a85a489df228f428d344df84b3440f3e6d
jangg0627 <jangg0627@naver.com>
2023-05-01 01:52:26 +0900

    🎯✨ 구현한 `FeaturedPosts` 삽입 - 현재 타입스크립트 관련 오류로 에러 처리 문구를 추가해야함 - 오류: `'Promise<Element>' is not a valid JSX element` - 오류 처리 문구: `{/* @ts-expect-error Server Component */}` - 관련정보: https://beta.nextjs.org/docs/configuring/typescript#async-server-component-typescript-error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
-
 import Hero from "@/components/Hero";
+import FeaturedPosts from "@/components/FeaturedPosts";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      {/* @ts-expect-error Server Component */}
+      <FeaturedPosts />
     </>
   );
 }

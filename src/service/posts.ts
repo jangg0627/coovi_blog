14819c61983052db3680a8e5d95bffa422fa3381
jangg0627 <jangg0627@naver.com>
2023-05-01 01:45:01 +0900

    🧠🔨 폴더에서 데이터를 받아오는 코드 작성 - Promise를 사용한 데이터 받아오기 - `sort`를 사용해서 날짜 순으로 정렬 - 이유는 모르겠으나 개발 과정중 Module not found: Can't resolve 'fs' 라는 오류가 발생    -> 코드 작성을 마치니 에러가 사라짐

---
+++ src/service/posts.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { readFile } from "fs/promises";
+// import { promises as fs } from "fs";
+
+export type Post = {
+  title: string;
+  description: string;
+  date: string;
+  category: string;
+  path: string;
+  featured: boolean;
+};
+
+export async function getAllPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  // const data = await fs.readFile(filePath, "utf-8");
+  // const posts = JSON.parse(data);
+  // return posts.sort((a: Post, b: Post) => (a.date > b.date ? -1 : 1));
+  return readFile(filePath, "utf-8")
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}

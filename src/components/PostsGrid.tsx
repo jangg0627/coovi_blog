f4066b880d116c9341bc0989cfccb3edd76df324
jangg0627 <jangg0627@naver.com>
2023-05-01 16:48:56 +0900

    ♻️✨ PostCard 컴포넌트 생성 - `FeaturedPosts` -> `PostsGrid` -> `PostCard` 순으로 props 전달 - 형식과 이름을 맞춘 이미지 파일을 호출해서 사용

--- src/components/PostsGrid.tsx
+++ src/components/PostsGrid.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/service/posts";
+import PostCard from "./PostCard";
 
 type Props = { posts: Post[] };
 
@@ -6,7 +7,11 @@ export default function PostsGrid({ posts }: Props) {
   return (
     <ul>
       {posts.map((post) => {
-        return <li key={post.path}>{post.title}</li>;
+        return (
+          <li key={post.path}>
+            <PostCard post={post} />
+          </li>
+        );
       })}
     </ul>
   );

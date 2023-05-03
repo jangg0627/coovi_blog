ebd223f5b253be83c0f38afbc54f27fb4080e457
jangg0627 <jangg0627@naver.com>
2023-05-03 22:52:41 +0900

    🥅  구조분해 할당 실수로 잘못된 값이 가고 있었음 - `target`을 es6로 올린것은 set 구조분해 할당은 es6부터 적용되기 때문에 버전을 올림 - 대부분의 사람은 ES6이상을 사용하므로 문제 없음

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,12 +4,7 @@ import FilterablePosts from "@/components/FilterablePosts";
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
-  const categories = [new Set(posts.map((post) => post.category))];
+  const categories = [...new Set(posts.map((post) => post.category))];
 
-  return (
-    <>
-      {/* @ts-expect-error Server Component */}
-      <FilterablePosts posts={posts} categories={categories} />
-    </>
-  );
+  return <FilterablePosts posts={posts} categories={categories} />;
 }

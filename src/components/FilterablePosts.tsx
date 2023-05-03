c2d0af77d0048fe8c6722fc613c580bb9f1c925a
jangg0627 <jangg0627@naver.com>
2023-05-03 22:54:39 +0900

    🧠✨ Post 페이지 로직 구현 - State와 Props를 주고 받아 구현

---
+++ src/components/FilterablePosts.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+import { Post } from "@/service/posts";
+import PostsGrid from "./PostsGrid";
+import Categories from "./Categories";
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+
+const ALL_POSTS = "All Posts";
+
+export default function FilterablePosts({ posts, categories }: Props) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filteredPosts =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+
+  // function handleClick(selected: string) {
+  //   setSelected(selected);
+  // }
+
+  return (
+    <section>
+      <PostsGrid posts={filteredPosts} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}

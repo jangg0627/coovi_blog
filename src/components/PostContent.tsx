67cfd27d4c19a971b6d5a0c7fc810a88069d75c8
jangg0627 <jangg0627@naver.com>
2023-05-05 05:49:40 +0900

    ✏️ 잘못된 쉼표 삭제

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -15,7 +15,7 @@ export default function PostContent({ post }: { post: PostData }) {
       <h1 className="text-4xl font-bold">{title}</h1>
       <p className="text-xl font-bold">{description}</p>
       <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
-      <MarkdownViewer content={content} />;
+      <MarkdownViewer content={content} />
     </section>
   );
 }

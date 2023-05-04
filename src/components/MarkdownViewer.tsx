02f3d7393bacde80fd4cadfe9529ffb230970d2a
jangg0627 <jangg0627@naver.com>
2023-05-04 17:49:16 +0900

    🧩🎯📦🥅 `react-syntax-highlighter` 패키지 사용 & 타입관련 패키지 설치 - `use client` 필요 - 공식문서를 보고 참고해 `react-markdown`에 `react-syntax-highlighter` 적용    -> 공식문서 링크      - https://github.com/remarkjs/react-markdown#use-custom-components-syntax-highlight - 타입에러 발생, 공식문서에서 타입 관련 에러 서칭 후 적용    -> `yarn add -D types/react-syntax-highlighter`    -> 공식문서 링크      - https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/479

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,9 +1,35 @@
+"use client";
+
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={dark}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </ReactMarkdown>
   );

c5e22f8b1b80ecb007276c2ac413123072f75337
jangg0627 <jangg0627@naver.com>
2023-05-04 17:25:59 +0900

    💄 개별 포스트 페이지 스타일링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -12,20 +12,22 @@ export default async function Page({ params: { slug } }: Props) {
   const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <article>
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
+        className="w-full h-1/6 max-h-[500px]"
         src={`/images/posts/${path}.png`}
         alt={`${title} image`}
         width={760}
         height={420}
       />
-      <section>
-        <div>
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
           <AiTwotoneCalendar />
-          <p>{date.toString()}</p>
+          <p className="font-semibold ml-2">{date.toString()}</p>
         </div>
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
         <MarkdownViewer content={content} />;
       </section>
     </article>

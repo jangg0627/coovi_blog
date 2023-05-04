bcc354d3cf4a4418e34e6976c318c2fe3f218cd0
jangg0627 <jangg0627@naver.com>
2023-05-05 00:47:11 +0900

    🎯📐 포스트 데이터를 받아서 출력 - `&&`으로 값 존재 여부를 확인

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -9,17 +9,22 @@ type Props = {
 
 export default async function Page({ params: { slug } }: Props) {
   const post = await getPostData(slug);
+  const { title, path, nextPost, prevPost } = post;
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
         className="w-full h-1/6 max-h-[500px]"
-        src={`/images/posts/${post.path}.png`}
-        alt={`${post.title} image`}
+        src={`/images/posts/${path}.png`}
+        alt={`${title} image`}
         width={760}
         height={420}
       />
       <PostContent post={post} />
+      <section>
+        {prevPost && <p>{prevPost.title}</p>}
+        {nextPost && <p>{nextPost.title}</p>}
+      </section>
     </article>
   );
 }

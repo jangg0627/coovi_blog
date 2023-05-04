b0102c89ef08e3d9fff5b5c38ec58840d4dfa36d
jangg0627 <jangg0627@naver.com>
2023-05-04 19:38:06 +0900

    ♻️ 코드 리펙토링 - post를 담당하는 부분을 component로 분리했음

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,35 +1,25 @@
 import Image from "next/image";
 
-import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
-import { AiTwotoneCalendar } from "react-icons/ai";
+import PostContent from "@/components/PostContent";
 
 type Props = {
   params: { slug: string };
 };
 
 export default async function Page({ params: { slug } }: Props) {
-  const { title, description, date, path, content } = await getPostData(slug);
+  const post = await getPostData(slug);
 
   return (
     <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
       <Image
         className="w-full h-1/6 max-h-[500px]"
-        src={`/images/posts/${path}.png`}
-        alt={`${title} image`}
+        src={`/images/posts/${post.path}.png`}
+        alt={`${post.title} image`}
         width={760}
         height={420}
       />
-      <section className="flex flex-col p-4">
-        <div className="flex items-center self-end text-sky-600">
-          <AiTwotoneCalendar />
-          <p className="font-semibold ml-2">{date.toString()}</p>
-        </div>
-        <h1 className="text-4xl font-bold">{title}</h1>
-        <p className="text-xl font-bold">{description}</p>
-        <div className="w-44 border-2 border-sky-600 mt-4 mb-8" />
-        <MarkdownViewer content={content} />;
-      </section>
+      <PostContent post={post} />
     </article>
   );
 }

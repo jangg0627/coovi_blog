a777d2a2d4e0a063569a0b14e3f4dfd392f9ae0c
jangg0627 <jangg0627@naver.com>
2023-05-05 17:47:58 +0900

    🔨 about페이지 구현

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,17 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "font-bold text-2xl text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <h1>This is AboutPage Component</h1>;
+  return (
+    <>
+      <Hero />
+      <section className="text-center bg-gray-100 p-8 m-8 shadow-lg">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>만드는것을 좋아하고 개발을 사랑하는 프론트 엔드 개발자</p>
+        <h2 className={TITLE_CLASS}>Stack</h2>
+        <p>React, TS</p>
+      </section>
+    </>
+  );
 }

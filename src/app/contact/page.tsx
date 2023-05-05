85c35c23679d18e494e11444e8384c346c80e360
jangg0627 <jangg0627@naver.com>
2023-05-05 22:11:02 +0900

    🚧 개발중인 ContactPage - 시간에 쪼들려서 일단은 보류 - 메일링 코드를 넣을 예정

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,29 @@
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+
+import Development from "@/components/Development";
+
+const LINKS = [{ icon: <AiFillGithub />, url: "https://github.com/jangg0627" }];
+
 export default function ContactPage() {
-  return <h2>This is ContactPage Component</h2>;
+  return (
+    <>
+      <Development isPage>
+        <h2>Contact Me!</h2>
+        <p>jangg0627@naver.com</p>
+        <ul>
+          {LINKS.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>{link.icon}</li>
+            </a>
+          ))}
+        </ul>
+        <h2>Or Send me an email</h2>
+      </Development>
+    </>
+  );
 }

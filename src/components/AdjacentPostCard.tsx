40dd6b941bea786f1a8afb36e45ece1eaeb97d1d
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-05-25 16:51:47 +0900

    변경사항적용 및 글 발행 (#4)
    * 🔥🩹 불필요한 이미지 삭제 & ESLint문법으로 수정
    - 사용하지 않는 이미지 삭제
    - ESLint에서 허용하지않는 ' 을 삭제함
    
    * 📦 `gray-matter` 설치
    - 마크다운에 메타 데이터를 담기위해 사용
    
    * 🧩🧠🔨🥅 데이터 로직 변경 & 자잘한 오류 수정
    - `gray matter`패키지를 활용한 md 파일 처리
       -> 마크다운 파일 내부에서 메타데이터 생성 & 처리
       -> 🥅 28번줄 `matterOptions`에서 date 속성을 JS date 타입으로 바꾸지 않게 설정
    - 🥅 42번줄 TypeScript as 문법을 사용해 내부에서 return 되는 값위 속성을 지정해서 타입 에러를 잡음
    
    * 🔥 필요없는 기존 더미 데이터 삭제
    
    * ✨ 다중 카테고리 기능 구현
    - md 메타데이터 자체를 배열로 작성
    - `posts.ts`에서 배열을 뿌리게 설정
    - 관련 데이터를 사용하는 코드들 변경
    
    * 💄✨🚧 언스플레쉬 이미지로 썸네일 사용하기 구현
    - 로컬이 아닌 언스플래쉬에서 이미지를 받아서 보여주는 기능을 구현함
    - 화면에 따라서 이미지가 자연스럽게 늘어나는 기능 구현
    - 이미지의 화질이 망가지는 것은 추후 해결할 과제
    
    * 📝 제로베이스 과제 블로그글 작성
    - 첫 블로그글! 야호!
    
    * ✏️🩹 간단한 오타 수정
    - 오타 수정
    - cSpell 단어등록
    
    * 🎯🔍 SEO 관련 작업
    - 네이버 서치어드바이저 등록
      -> 일반적인 meta데이터가 아닌 데이터는 other에 등록해야지만 이러한 meta태그 적용가능
    - `robots.txt` 설정
    - 블로그의 포스트를 자동으로 조회해서 sitemap에 등록하는 기능 구현(동적 sitemap)
      -> 포스트 외에 링크는 직접 등록해줌
    
    * 🍱 파비콘 등록
    - 친구에게 부탁해서 만든 아이콘
    - 아리가또!
    
    * 🔍 구글 서치콘솔 등록용 태그
    - 마친가지로 other을 통해 등록
    
    * ⬆️ NEXT.js 버전 업그레이드
    - app router 기능이 안정화 된 13.4 버전으로 업데이트함
    
    * 🩹 내부에서 참고하는 썸네일이미지 로직 변경
    - 기존에 폴더에서 불러오던 이미지에서 언스플래쉬를 사용하는 방식으로 수정
    
    * 📝 제로베이스 과제 블로그글 작성
    - 프론트엔드와 백엔드에 관해
    
    ---------
    
    Co-authored-by: jangg0627 <jangg06270@gmail.com>

--- src/components/AdjacentPostCard.tsx
+++ src/components/AdjacentPostCard.tsx
@@ -13,14 +13,14 @@ const ICON_CLASS =
   "text-5xl m-4 text-yellow-300 transition-all group-hover:text-6xl";
 
 export default function AdjacentPostCard({
-  post: { path, title, description },
+  post: { path, title, description, unsplashThumbnail },
   type,
 }: Props) {
   return (
     <Link href={`/posts/${path}`} className="relative w-full bg-black max-h-56">
       <Image
         className="w-full opacity-40 "
-        src={`/images/posts/${path}.png`}
+        src={unsplashThumbnail}
         alt={`${title} 이미지`}
         width={150}
         height={100}

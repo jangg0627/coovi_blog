cfab07e5c4498a750ec0f0e315813720821d1814
jangg0627 <jangg06270@gmail.com>
2023-06-09 14:26:47 +0900

    🐛 카테고리 중첩 출력 해결 - `gary matter`에서 배열로 내보내는 categories를 그대로 받아서 카테고리가 중첩돼던 현상을 해결함 - `new Set`으로 중복 요소를 없애기 전에 `flat()`으로 중첩 배열을 풀어줌

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,7 +4,8 @@ import FilterablePosts from "@/components/FilterablePosts";
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
-  const categories = [...new Set(posts.map((post) => post.categories))];
+  const categories = [...new Set(posts.map((post) => post.categories).flat())];
+  console.log(categories);
 
   return <FilterablePosts posts={posts} categories={categories.flat()} />;
 }

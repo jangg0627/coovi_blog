85994304276a1bea7d621bef9c2d045d6829d2be
jangg0627 <jangg06270@gmail.com>
2023-06-09 14:34:48 +0900

    🐛💄 postcard 디자인 수정(글자 줄바꿈방지, image 컨테이너 지정) - 일정한 크기를 유지하는 컨테이너 생성(`div`) - `Image` 자체에 크기는 무시하고 w, h는 최대로 키운뒤 `object-cover` 처리로 이미지를 꽉 차게 만듦

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -10,23 +10,26 @@ export default function PostCard({
 }: Props) {
   return (
     <Link href={`/posts/${path}`}>
-      <article className="rounded-md overflow-hidden shadow-lg">
-        <Image
-          className="w-full"
-          src={unsplashThumbnail}
-          alt={title}
-          width={300}
-          height={200}
-        />
-        <div className="flex flex-col items-center p-4">
-          <time className="self-end">{date.toString()}</time>
-          <h3 className="text-lg font-bold">{title}</h3>
-          <p className="w-full truncate text-center">{description}</p>
+      <article className="overflow-hidden rounded-md shadow-lg">
+        <div className="relative h-40 overflow-hidden w-62">
+          <Image
+            src={unsplashThumbnail}
+            alt={title}
+            fill
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="flex flex-col items-center p-2">
+          <time className="self-end text-xs">{date.toString()}</time>
+          <h3 className="w-full text-lg font-bold text-center truncate">
+            {title}
+          </h3>
+          <p className="w-full text-center truncate">{description}</p>
           <div>
             {categories.map((category) => (
               <span
                 key={category}
-                className="text-sm rounded-lg bg-green-200 px-2 my-2 mx-1"
+                className="px-2 mx-1 my-2 text-sm bg-green-200 rounded-lg"
               >
                 {category}
               </span>
